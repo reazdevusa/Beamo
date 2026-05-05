7a66fadf2e28a329afabdb437077504b55a0b33d
alfareaz <alfa.reaz@gmail.com>
2026-05-05 05:56:38 -0400

    HealthController.cs file added.

---
+++ Beamo.Api/Controllers/HealthController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get() => Ok("Healthy");
+}
